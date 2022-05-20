1ac033428d893d04565e0c1ec7ad063165d3eaf4
Steven <steven@houtenbos.nl>
2022-05-20 08:40:17 +0200

    Websocket messages are now logged to console

--- stratviz-webapp/server/server.js
+++ stratviz-webapp/server/server.js
@@ -9,8 +9,10 @@ const io = require('socket.io')(3001, {
 io.on('connection', socket => {
     // what happens if we get a message of type connection
     console.log(socket.id)
-})
 
-io.on('hello', () => {
-    // What happens when this type of message is received
+    // What happens when a can message comes in
+    socket.on('canMessage', (car, timestamp, message) => {
+        // When canMessage comes in, log its information to the console.
+        console.log(`Message received from ${car} at ${timestamp}: ${message}`)
+    })
 })
